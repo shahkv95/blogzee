49fc8a0b6fdd78c715d0642a4850d54f6fc60d0a
shahkv95 <kushshah253@gmail.com>
2023-02-26 15:49:55 +0530

    Updated compare_latency.py

--- connections/client/compare_latency.py
+++ connections/client/compare_latency.py
@@ -1,20 +1,34 @@
 import time
+from typing import Callable
 
-def compare_latency(nc, llc, num_runs=10):
-    nc_latency = []
+
+def compare_latency(func1: Callable, func2: Callable, num_runs: int = 10) -> None:
+    """
+    Compares the latency of two functions.
+
+    Parameters:
+    func1 (Callable): The first function to compare.
+    func2 (Callable): The second function to compare.
+    num_runs (int): The number of times to run each function to get an average latency.
+    """
+
+    # Get the latency of the first function
+    func1_latency = []
     for i in range(num_runs):
         start_time = time.perf_counter()
-        nc()
+        func1()
         end_time = time.perf_counter()
-        nc_latency.append((end_time - start_time))
+        func1_latency.append((end_time - start_time))
 
-    llc_latency = []
+    # Get the latency of the second function
+    func2_latency = []
     for i in range(num_runs):
         start_time = time.perf_counter()
-        llc()
+        func2()
         end_time = time.perf_counter()
-        llc_latency.append((end_time - start_time))
-    print("----------------------------------------------------------------")
-    print(f'\n{"Connection Type":<30} {"Latency (s)":<15}')
-    print(f'{f"New Connection":<30} {f"{sum(nc_latency)/len(nc_latency):.5f}":<15}')
-    print(f'{f"Long-Lived Connection":<30} {f"{sum(llc_latency)/len(llc_latency):.5f}":<15}')
+        func2_latency.append((end_time - start_time))
+
+    # Print the results
+    print(f'{"Function":<30} {"Latency":<15}')
+    print(f'{f"{func1.__name__}":<30} {f"{sum(func1_latency)/len(func1_latency):.5f}":<15}')
+    print(f'{f"{func2.__name__}":<30} {f"{sum(func2_latency)/len(func2_latency):.5f}":<15}')
